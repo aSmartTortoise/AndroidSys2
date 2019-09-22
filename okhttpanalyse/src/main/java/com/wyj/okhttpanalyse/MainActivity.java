9ec1fb544e1cc89329f6531e3d1b675ce285d5f8
aSmartTortoise <wyjonneteasy@163.com>
2019-09-22 22:21:56 +0800

    分析okhttp 3.7.0源码

---
+++ okhttpanalyse/src/main/java/com/wyj/okhttpanalyse/MainActivity.java
@@ -0,0 +1,44 @@
+package com.wyj.okhttpanalyse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        OkHttpClient client = new OkHttpClient();
+        String url = "http://app.ekaobang.com/" + "webschool/findStartAd.html";
+        Request request = new Request
+                .Builder()
+                .url(url)
+                .build();
+        try {
+//            Response response = client.newCall(request).execute();
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
